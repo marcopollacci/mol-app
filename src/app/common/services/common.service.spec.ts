19984ba03ec6c5c166e6d67d13ced99158894eb9
Marco Pollacci <marco.pollacci@gellify.com>
2025-03-19 14:15:42 +0100

    test(project): :white_check_mark: update test

--- src/app/common/services/common.service.spec.ts
+++ src/app/common/services/common.service.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
+import { provideHttpClient } from '@angular/common/http';
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { CommonService } from './common.service';
 
 describe('CommonService', () => {
   let service: CommonService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideExperimentalZonelessChangeDetection(),
+        provideHttpClient(),
+      ],
+    });
     service = TestBed.inject(CommonService);
   });
 
--- src/app/core/components/home/home.component.spec.ts
+++ src/app/core/components/home/home.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
@@ -8,9 +9,9 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
-    })
-    .compileComponents();
+      providers: [provideExperimentalZonelessChangeDetection()],
+      imports: [HomeComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
