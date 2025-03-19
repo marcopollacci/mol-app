4c4c991c3cfe3fa6dbd582398f6c9534b688d8bb
Marco Pollacci <marco.pollacci@gellify.com>
2025-03-19 15:29:09 +0100

    test(project): :white_check_mark: adding some test

--- src/app/features/clients/components/home-clients/home-clients.component.spec.ts
+++ src/app/features/clients/components/home-clients/home-clients.component.spec.ts
@@ -1,17 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { provideHttpClient } from '@angular/common/http';
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { ClientsService } from '@features/clients/services/clients.service';
 import { HomeClientsComponent } from './home-clients.component';
 
 describe('HomeClientsComponent', () => {
   let component: HomeClientsComponent;
   let fixture: ComponentFixture<HomeClientsComponent>;
+  let clientService: ClientsService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeClientsComponent]
-    })
-    .compileComponents();
+      imports: [HomeClientsComponent],
+      providers: [
+        provideExperimentalZonelessChangeDetection(),
+        provideHttpClient(),
+      ],
+    }).compileComponents();
 
+    clientService = TestBed.inject(ClientsService);
     fixture = TestBed.createComponent(HomeClientsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +27,13 @@ describe('HomeClientsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get clients', () => {
+    const spy = spyOn(clientService, 'getAllClients').and.callThrough();
+    fixture = TestBed.createComponent(HomeClientsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    expect(spy).toHaveBeenCalled();
   });
 });
--- src/app/features/clients/services/clients.service.spec.ts
+++ src/app/features/clients/services/clients.service.spec.ts
@@ -1,16 +1,32 @@
 import { TestBed } from '@angular/core/testing';
 
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { ClientsService } from './clients.service';
 
 describe('ClientsService', () => {
   let service: ClientsService;
-
+  let http: HttpClient;
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideExperimentalZonelessChangeDetection(),
+        provideHttpClient(),
+      ],
+    });
+    http = TestBed.inject(HttpClient);
     service = TestBed.inject(ClientsService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('return empty array if error', () => {
+    TestBed.runInInjectionContext(() => {
+      spyOn(http, 'get').and.throwError('error');
+      const result = service.getAllClients();
+      expect(result.value()).toEqual([]);
+    });
   });
 });
